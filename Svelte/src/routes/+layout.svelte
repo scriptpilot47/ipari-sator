ef26a534843599dfa176ce01b50170dab96e861f
Sam <sam@debian-desktop.local>
2025-09-20 11:39:02 +0200

    added a function to auto close hamburger menu on mobile

--- Svelte/src/routes/+layout.svelte
+++ Svelte/src/routes/+layout.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
 	import '../app.css';
+  import { onMount } from 'svelte';
 	import favicon from '$lib/assets/favicon.svg';
   import Logo from '../components/Logo.svelte';
   import Footer from '../components/Footer.svelte';
 	import { page } from '$app/state';
+	
+  let { children } = $props();
 
-	let { children } = $props();
+  // mobile menu functions
+  let width = $state(0)
+    function updateSize() {
+    width = window.innerWidth;
+    const drawerCheckbox = document.getElementById('my-drawer-3');
+    if (width > 1024 && drawerCheckbox && drawerCheckbox.checked === true) {
+      drawerCheckbox.checked = false;
+    }
+  }
+
+  function closeMenu() {
+   const drawerCheckbox = document.getElementById('my-drawer-3');
+    if (drawerCheckbox) drawerCheckbox.checked = false;
+  }
+    onMount(() => {
+    updateSize(); // initial size
+    window.addEventListener('resize', updateSize);
+
+    return () => {
+      window.removeEventListener('resize', updateSize);
+    };
+  });
+
 </script>
 
 <svelte:head>
@@ -17,7 +42,7 @@
     <!-- Navbar -->
     <div class="navbar bg-primary w-full border-b-3 border-solid border-[#ecf9ff]">
       <div class="flex-none lg:hidden">
-        <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost hover:!bg-transparent">
+        <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost hover:!bg-transparent active:!bg-transparent">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -57,11 +82,11 @@
     <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
     <ul class="menu bg-base-200 min-h-full w-80 px-4 py-8 text-2xl">
       <!-- Sidebar content here -->
-      	<li><a href="/" class={`${page.url.pathname === '/' ? 'text-primary' : ''} active:!bg-transparent`}>Kezdőlap</a></li>
-      	<li><a href="/products"class={`${page.url.pathname === '/products' ? 'text-primary' : ''} active:!bg-transparent`}>Termékek</a></li>
-      	<li><a href="/prices"class={`${page.url.pathname === '/prices' ? 'text-primary' : ''} active:!bg-transparent`}>Arak</a></li>
-        <li><a href="/blog"class={`${page.url.pathname === '/blog' ? 'text-primary' : ''} active:!bg-transparent`}>Blog</a></li>
-      	<li><a href="/contact"class={`${page.url.pathname === '/contact' ? 'text-primary' : ''} active:!bg-transparent`}>Kapcsolat</a></li>
+      	<li><a href="/" class={`${page.url.pathname === '/' ? 'text-primary' : ''} active:!bg-transparent`} onclick={closeMenu}>Kezdőlap</a></li>
+      	<li><a href="/products"class={`${page.url.pathname === '/products' ? 'text-primary' : ''} active:!bg-transparent`} onclick={closeMenu}>Termékek</a></li>
+      	<li><a href="/prices"class={`${page.url.pathname === '/prices' ? 'text-primary' : ''} active:!bg-transparent`} onclick={closeMenu}>Arak</a></li>
+        <li><a href="/blog"class={`${page.url.pathname === '/blog' ? 'text-primary' : ''} active:!bg-transparent`} onclick={closeMenu}>Blog</a></li>
+      	<li><a href="/contact"class={`${page.url.pathname === '/contact' ? 'text-primary' : ''} active:!bg-transparent`} onclick={closeMenu}>Kapcsolat</a></li>
     </ul>
   </div>
 </div>
