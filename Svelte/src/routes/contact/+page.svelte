9ef4aa5c4234a6ac82d0a5a18e6d36ed0bf03f15
Sam <sam@debian-desktop.local>
2025-10-16 12:08:16 +0200

    changed contact page design

--- Svelte/src/routes/contact/+page.svelte
+++ Svelte/src/routes/contact/+page.svelte
@@ -10,10 +10,21 @@
 		<Location />
 	</div>
 	<div class="max-w-250 mx-auto">
-		<ul class="px-4 py-4 flex flex-col gap-1">
-			<li class="text-xl md:text-2xl text-primary">+36 20 234 5678</li>
-			<li class="text-xl md:text-2xl">iparisator@gmail.com</li>
-			<li class="text-xl md:text-2xl">2112 Veresegyhaz hajo utca 8.</li>
+		<ul class="px-4 py-6 flex flex-col gap-3 text-lg md:text-xl">
+			<li>
+				☎️ <span class="font-semibold text-primary">Telefon:</span>
+				<a href="tel:+36202345678" class="ml-2 font-medium hover:underline"> +36 20 234 5678 </a>
+			</li>
+			<li>
+				✉️ <span class="font-semibold text-primary">E-mail:</span>
+				<a href="mailto:iparisator@gmail.com" class="ml-2 hover:text-yellow-400 transition">
+					iparisator@gmail.com
+				</a>
+			</li>
+			<li>
+				📍 <span class="font-semibold text-primary">Cím:</span>
+				<span class="ml-2">2112 Veresegyház, Hajó utca 8.</span>
+			</li>
 		</ul>
 	</div>
 </div>
